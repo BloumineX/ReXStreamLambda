635d7281b5918a3fe48623c447bf9b35cca42cb1
BloumiX <xavier.bloumine@gmail.com>
2016-04-08 15:04:38 +0200

    github site

--- ExempleStreamLambda/src/test/java/PrinterList.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by xavierbloumine on 07/04/2016.
- */
-public class PrinterList {
-    public static void print(Integer i) {
-        System.out.println(i);
-    }
-}
-
--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# TD
 
-## Slides
-http://BloumineX.github.io/RexStreamLambda
-
-## Licence
-
-MIT Licensed
